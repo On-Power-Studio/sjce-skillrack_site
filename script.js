8a876aa42b456531cc7ee1ea27f81dcd59c81242
On-Power-Studio <112738105+On-Power-Studio@users.noreply.github.com>
2023-10-20 15:47:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,28 +3,41 @@ const googleSheetAPI = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTMlDHyV
 
 // Function to fetch and display student data
 async function fetchStudentData() {
-    try {
-        const response = await fetch(googleSheetAPI);
-        const data = await response.json();
+  try {
+    // Fetch the student data from the Google Sheet
+    const response = await fetch(googleSheetAPI);
 
-        // Sort data by skill scores
-        data.sort((a, b) => b.score - a.score);
-
-        const studentList = document.getElementById('student-list');
-
-        data.forEach(student => {
-            studentList.innerHTML += `
-                <div>
-                    <img src="${student.image}" alt="${student.name}">
-                    <p>Name: ${student.name}</p>
-                    <p>Department: ${student.department}</p>
-                    <p>SkillRack Score: ${student.score}</p>
-                </div>
-            `;
-        });
-    } catch (error) {
-        console.error('Error fetching data:', error);
+    // Check if the response was successful
+    if (!response.ok) {
+      throw new Error('Error fetching data:', response.status);
     }
+
+    // Convert the response to JSON
+    const data = await response.json();
+
+    // Sort the student data by skill scores
+    data.sort((a, b) => b.score - a.score);
+
+    // Get the student list element
+    const studentList = document.getElementById('student-list');
+
+    // Clear the student list element
+    studentList.innerHTML = '';
+
+    // Iterate over the student data and add each student to the list
+    data.forEach(student => {
+      studentList.innerHTML += `
+        <div>
+          <img src="${student.image}" alt="${student.name}">
+          <p>Name: ${student.name}</p>
+          <p>Department: ${student.department}</p>
+          <p>SkillRack Score: ${student.score}</p>
+        </div>
+      `;
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // Periodically update data
